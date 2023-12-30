29869a273be78331faea638146f491f34b992529
mo-adel-97 <113055631+mo-adel-97@users.noreply.github.com>
2023-12-30 11:38:38 +0200

    add to settings core
    add to settings core

--- reader.js
+++ reader.js
@@ -7,6 +7,13 @@ const app = express();
 app.use(express.json());
 app.use(cors()); // Enable CORS for all routes
 
+// Configure CORS to allow requests from your React app (replace localhost:3000 with your actual React app's URL during development)
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+}));
+
 app.use(scrapeRoutes);
 
 const PORT = 3001;
